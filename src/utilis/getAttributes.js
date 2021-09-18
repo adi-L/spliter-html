d9ec976d96e943ef8937e768dd2965c6e096985b
style-it <styleit.api@gmail.com>
2021-09-18 20:04:21 +0300

    not get attrs from void elements

--- src/utilis/getAttributes.js
+++ src/utilis/getAttributes.js
@@ -1,12 +1,13 @@
 import { ischildOf } from './ischildOf';
 
 export function getAttributes(fromElement, toElement) {
+  const nonValidTags = ['IMG', 'A', 'BR', 'HR', 'INPUT', 'AUDIO', 'VIDEO', 'PICTURE'];
   let attributes = {};
   let currectElement = fromElement;
   while (currectElement && ischildOf(currectElement, toElement.parentElement)) {
     let attrs = currectElement.attributes;
     for (let i = attrs.length - 1; i >= 0; i--) {
-      if (attrs[i].name !== 'style') {
+      if (attrs[i].name !== 'style' && !nonValidTags.includes(currectElement.nodeName)) {
         if (!attributes[attrs[i].name]) {
           attributes[attrs[i].name] = attrs[i].value;
         }
